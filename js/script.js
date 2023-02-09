48a01fc62ffab903f3b84ae7cb8b7973b31552b9
Mari Sumida Miyashiro <mariksm@hawaii.edu>
2023-02-09 00:16:08 -1000

    started adding content

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    mousewheel: true,
+    loop: true,
+    effect: 'fade',
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        
+    },
+
+});
