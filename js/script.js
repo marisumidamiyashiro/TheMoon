028fe27bc4b7e3bcbedfe65ded2baa62adb9b3f0
Mari Sumida Miyashiro <mariksm@hawaii.edu>
2023-03-21 17:00:18 -1000

    added links to each moon phase in header.

--- js/script.js
+++ js/script.js
@@ -5,15 +5,18 @@ const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     mousewheel: true,
     loop: true,
-    effect: 'slide',
+    effect: 'fade',
+    fadeEffects: {
+        crossFade: true
+    },
+    hashNavigation: {
+        watchState: true,
+    },
     
-
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
-
-// SCROLL-TRIGGERED ANIMATIONS
 
 });
 
