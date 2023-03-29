94d6d658a8d3640e3792947ed979c3214ad10a4e
Mari Sumida Miyashiro <mariksm@hawaii.edu>
2023-03-28 20:29:54 -1000

    added preloader

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 
+$(document).ready(function() {
+
+    setTimeout(function() {
+        $('#ctn-preloader').addClass('loaded');
+        $('body').removeClass('no-scroll-y');
+
+    if ($('#ctn-preloader').hasClass('loaded')) {
+
+        $('#preloader').delay(2000).queue(function() {
+            $(this).remove();
+        });
+        }
+    }, 3750);
+    
+});
+
 // SWIPERJS 
 
 const swiper = new Swiper('.swiper', {
